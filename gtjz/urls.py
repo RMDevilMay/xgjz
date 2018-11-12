844e8619478ebbb49432a59de1081a5dabf51201
rmdevilmay@gmail.com <rmdevilmay@gmail.com>
2018-11-13 02:09:54 +0800

    add

--- gtjz/urls.py
+++ gtjz/urls.py
@@ -1,4 +1,5 @@
-"""xgjz URL Configuration
+# -*- coding: utf-8 -*-
+"""kst_web_services URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -10,12 +11,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^gtjz/', include('gtjz.urls')),
+	url(r'^$',views.index,name = "index"),
+	url(r'^index$', views.index, name = "index"),
 ]
